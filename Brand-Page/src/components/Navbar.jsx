a74aad04b7743caa34584063d80e4b23f85bf2d4
Debojyoti <khanradebojyoti@gmail.com>
2026-02-01 03:31:40 +0530

    Brand-Page: Version 1.0

---
+++ Brand-Page/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="/images/brand_logo.png" alt="logo" />
+            </div>
+            <ul>
+                <li href="#">MENU</li>
+                <li href="#">LOCATION</li>
+                <li href="#">ABOUT</li>
+                <li href="#">CONTACT</li>
+            </ul>
+
+            <button>Login</button>
+        </nav>
+    );
+};
+
+export default Navbar;
